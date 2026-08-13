19a6bfbc963a28d0e3c803bbb91ac9a926f26da7
pranita-hubInt <pranita@hubinterior.com>
2026-08-13 13:12:16 +0530

    Resolve ecom changes

--- src/main/java/com/hubinterior/Ecom/Homes/merry/Domain/product/controller/PricingController.java
+++ src/main/java/com/hubinterior/Ecom/Homes/merry/Domain/product/controller/PricingController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/pricing")
 @RequiredArgsConstructor
@@ -22,4 +24,27 @@ public class PricingController {
             @Valid @RequestBody Pricing_Req_DTO req, @PathVariable Long prod_id) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.updatePricing(req, prod_id));
     }
+
+//    @GetMapping("/getAllPricing")
+//    public ResponseEntity<List<Pricing_Res_DTO>> getAllPricing() {
+//        return ResponseEntity.ok(service.getAllPricing());
+//    }
+//
+//    @GetMapping("/getPricing/{price_id}")
+//    public ResponseEntity<Pricing_Res_DTO> getPricingById(@PathVariable Integer price_id) {
+//        return ResponseEntity.ok(service.getPricingById(price_id));
+//    }
+//
+//    @PutMapping("/updatePricing/{price_id}")
+//    public ResponseEntity<Pricing_Res_DTO> updatePricing(
+//            @PathVariable Integer price_id,
+//            @Valid @RequestBody Pricing_Req_DTO req) {
+//        return ResponseEntity.ok(service.updatePricing(price_id, req));
+//    }
+//
+//    @DeleteMapping("/deletePricing/{price_id}")
+//    public ResponseEntity<String> deletePricing(@PathVariable Integer price_id) {
+//        service.deletePricing(price_id);
+//        return ResponseEntity.ok("Pricing with id " + price_id + " deleted successfully.");
+//    }
 }
