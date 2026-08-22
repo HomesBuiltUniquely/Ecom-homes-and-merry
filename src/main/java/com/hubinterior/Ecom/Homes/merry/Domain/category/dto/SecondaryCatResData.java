4fe55b5c594bb58b1eaf824b3da1ff6e1c114941
HomesBuiltUniquely <tech@homesunderbudget.com>
2026-08-22 15:07:11 +0530

    retreving name matching  changes from the Ecom branch

--- src/main/java/com/hubinterior/Ecom/Homes/merry/Domain/category/dto/SecondaryCatResData.java
+++ src/main/java/com/hubinterior/Ecom/Homes/merry/Domain/category/dto/SecondaryCatResData.java
@@ -1,11 +1,14 @@
 package com.hubinterior.Ecom.Homes.merry.Domain.category.dto;
 
+import com.hubinterior.Ecom.Homes.merry.Domain.category.model.SecondaryCategory;
+
 import java.util.List;
 
 public record SecondaryCatResData(
         Long secondaryCategoryId,
         String secondaryCategoryName,
         String secondaryCategoryDescription,
-        List<SecondaryCatResData> subCategory
+        List<SecondaryCatResData> subCategory,
+        SecondaryCategory parent
 ) {
 }
