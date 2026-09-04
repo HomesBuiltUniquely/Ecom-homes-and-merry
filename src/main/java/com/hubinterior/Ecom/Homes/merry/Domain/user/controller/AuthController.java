b36db295cc05af38c0f2cdf157a84b8a12ea93d5
preranahuddar123 <prerana@hubinterior.com>
2026-09-04 11:30:43 +0530

    Update auth response with MessageResponse

--- src/main/java/com/hubinterior/Ecom/Homes/merry/Domain/user/controller/AuthController.java
+++ src/main/java/com/hubinterior/Ecom/Homes/merry/Domain/user/controller/AuthController.java
@@ -1,9 +1,5 @@
 package com.hubinterior.Ecom.Homes.merry.Domain.user.controller;
 
-import com.hubinterior.Ecom.Homes.merry.Common.JwtUtil;
-import com.hubinterior.Ecom.Homes.merry.Domain.user.Mapper.LoginMapper;
-import com.hubinterior.Ecom.Homes.merry.Domain.user.dto.AuthResponse;
-import com.hubinterior.Ecom.Homes.merry.Domain.user.dto.LoginRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,6 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hubinterior.Ecom.Homes.merry.Common.JwtUtil;
+import com.hubinterior.Ecom.Homes.merry.Domain.common.dto.MessageResponse;
+import com.hubinterior.Ecom.Homes.merry.Domain.user.Mapper.LoginMapper;
+import com.hubinterior.Ecom.Homes.merry.Domain.user.dto.AuthResponse;
+import com.hubinterior.Ecom.Homes.merry.Domain.user.dto.LoginRequest;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -27,7 +29,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<MessageResponse<AuthResponse>> login(@RequestBody LoginRequest request) {
 
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.Username(), request.Password())
@@ -36,7 +38,6 @@ public class AuthController {
 
         String token = jwtUtil.generateToken(request.Username());
 
-
-        return ResponseEntity.ok(loginmapper.toResponseDto(token,request.Username()));
+        return ResponseEntity.ok(new MessageResponse<>("Login successful.", loginmapper.toResponseDto(token, request.Username())));
     }
 }
